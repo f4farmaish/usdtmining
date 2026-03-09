9428aa76657146372cc6561267ecc09cc39c26ae
Farmaish Ali <farmaishalifaf@gmail.com>
2026-03-09 22:49:57 +0500

    Update Vite base path and fix backend URL

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // if using React
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './', // important for Netlify to serve scripts correctly
+});
